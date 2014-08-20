f0c2cffeb024f112d8e22d24b2aab213bc4453e7
Kenny Campagna <kennyc1012@gmail.com>
2014-08-20 18:39:14 -0400

    ProfileActivity reworked for fragment

---
+++ src/main/java/com/kenny/openimgur/ProfileActivity.java
@@ -0,0 +1,50 @@
+package com.kenny.openimgur;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.kenny.openimgur.fragments.ProfileFragment;
+
+/**
+ * Empty activity needed to display the profile fragment. This will be used for profiles that are not accessed from the main screen
+ * Created by kcampagna on 7/26/14.
+ */
+public class ProfileActivity extends BaseActivity {
+    private static final String KEY_USERNAME = "username";
+
+    public static Intent createIntent(Context context, @Nullable String user) {
+        Intent intent = new Intent(context, ProfileActivity.class);
+
+        if (!TextUtils.isEmpty(user)) {
+            intent.putExtra(KEY_USERNAME, user);
+        }
+
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.empty_header);
+        ActionBar ab = getActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowHomeEnabled(true);
+
+        if (savedInstanceState == null) {
+            Intent data = getIntent();
+            String username = null;
+
+            if (data != null && data.hasExtra(KEY_USERNAME)) {
+                username = data.getStringExtra(KEY_USERNAME);
+            } else if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+                username = data.getData().getPathSegments().get(1);
+            }
+
+            getFragmentManager().beginTransaction().add(R.id.content, ProfileFragment.createInstance(username, false)).commit();
+        }
+    }
+}
