d2fd091339c4a7ab1e26219b07eec59cc57dd927
Kennyc1012 <kennyc1012@gmail.com>
2015-04-12 15:03:22 -0400

    Don't animate drawer toggle
    - Don't animate the drawer toggle when the navigation drawer is
    opened/closed
    - Clean up the themes

--- src/main/java/com/kenny/openimgur/activities/MainActivity.java
+++ src/main/java/com/kenny/openimgur/activities/MainActivity.java
@@ -1,17 +1,11 @@
 package com.kenny.openimgur.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
@@ -19,9 +13,6 @@ import android.text.format.DateUtils;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AnimationUtils;
-import android.view.animation.OvershootInterpolator;
 import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 
