361637501902f62ab8bd505d96688a76dbec4d23
Kenny Campagna <kennyc1012@gmail.com>
2015-11-13 14:35:01 -0500

    Fix Vector Color

--- src/main/java/com/kenny/openimgur/fragments/CommentPopupFragment.java
+++ src/main/java/com/kenny/openimgur/fragments/CommentPopupFragment.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextUtils;
