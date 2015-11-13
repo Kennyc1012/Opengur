75dfd1a6838a546fd1d18b82c0c2a701e0a891d0
Kenny Campagna <kennyc1012@gmail.com>
2015-11-13 13:35:30 -0500

    Add Behavior for Fab

---
+++ src/main/java/com/kenny/openimgur/ui/FabBehavior.java
@@ -0,0 +1,33 @@
+package com.kenny.openimgur.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kcampagna on 11/13/15.
+ */
+public class FabBehavior extends FloatingActionButton.Behavior {
+
+    public FabBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0) {
+            child.hide();
+        } else if (dyConsumed < 0) {
+            child.show();
+        }
+    }
+}
