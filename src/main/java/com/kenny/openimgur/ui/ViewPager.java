ce0259834f86e929648555ef2087af934cab3b6e
Kenny Campagna <kennyc1012@gmail.com>
2015-12-21 13:57:41 -0500

    Allow ViewPager to Disable Swiping

--- src/main/java/com/kenny/openimgur/ui/ViewPager.java
+++ src/main/java/com/kenny/openimgur/ui/ViewPager.java
@@ -1,24 +1,41 @@
 package com.kenny.openimgur.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import com.kenny.openimgur.R;
 
 /**
  * Created by kcampagna on 6/30/15.
  */
 public class ViewPager extends android.support.v4.view.ViewPager {
 
+    private boolean mIsSwipingEnabled = true;
+
     public ViewPager(Context context) {
         super(context);
+        init(null);
     }
 
     public ViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MultiStateView);
+            mIsSwipingEnabled = a.getBoolean(R.styleable.ViewPager_swipingEnabled, true);
+            a.recycle();
+        }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (!mIsSwipingEnabled) return false;
+
         try {
             return super.onInterceptTouchEvent(ev);
         } catch (Exception e) {
@@ -26,5 +43,14 @@ public class ViewPager extends android.support.v4.view.ViewPager {
         }
 
         return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return mIsSwipingEnabled && super.onTouchEvent(ev);
+    }
+
+    public void setSwiping(boolean enabled) {
+        mIsSwipingEnabled = enabled;
     }
 }
