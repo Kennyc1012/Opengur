46dd0372f22df859062a41ede9077d6ed344777a
Kenny Campagna <kennyc1012@gmail.com>
2016-04-28 13:08:41 -0400

    Cleanup Theme Code

--- src/main/java/com/kenny/openimgur/classes/OpengurApp.java
+++ src/main/java/com/kenny/openimgur/classes/OpengurApp.java
@@ -52,10 +52,12 @@ public class OpengurApp extends Application implements SharedPreferences.OnShare
         stopUserManagerLeak();
         mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         mPref.registerOnSharedPreferenceChangeListener(this);
+        mTheme = ImgurTheme.fromPreferences(getResources(), mPref.getInt(SettingsActivity.KEY_THEME_NEW, ImgurTheme.GREY.primaryColor));
+        mTheme.isDarkTheme = mPref.getBoolean(SettingsActivity.KEY_DARK_THEME, true);
         mUser = SqlHelper.getInstance(this).getUser();
         if (mUser != null) AlarmReceiver.createNotificationAlarm(this);
         ImageUtil.initImageLoader(getApplicationContext());
-        migrateThemePreference();
+
         migrateDownloadFolder();
         FabricUtil.init(this, mPref);
 
@@ -209,22 +211,6 @@ public class OpengurApp extends Application implements SharedPreferences.OnShare
                 }
                 break;
         }
-    }
-
-    /**
-     * TODO Remove after several versions
-     */
-    private void migrateThemePreference() {
-        if (mPref.contains(SettingsActivity.KEY_THEME)) {
-            mTheme = ImgurTheme.getThemeFromString(mPref.getString(SettingsActivity.KEY_THEME, ImgurTheme.GREY.themeName));
-            mPref.edit()
-                    .putInt(SettingsActivity.KEY_THEME_NEW, mTheme.primaryColor)
-                    .remove(SettingsActivity.KEY_THEME).apply();
-        } else {
-            mTheme = ImgurTheme.fromPreferences(getResources(), mPref.getInt(SettingsActivity.KEY_THEME_NEW, ImgurTheme.GREY.primaryColor));
-        }
-
-        mTheme.isDarkTheme = mPref.getBoolean(SettingsActivity.KEY_DARK_THEME, true);
     }
 
     /**
